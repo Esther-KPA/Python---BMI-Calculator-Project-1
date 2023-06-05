f8fc05be62cac7a66ddb7b7ca149b0881a0dc579
Esther-KPA <110975877+Esther-KPA@users.noreply.github.com>
2023-06-05 15:05:44 +0200

    Update BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,3 +1,8 @@
+BMI Calculator
+
+https://mercer-health.com/services/weight-management-center/bmi-calculator#:~:text=Body%20Mass%20Index%2C%20or%20BMI,inches%20x%20height%20in%20inches
+
+
 #!/usr/bin/env python
 # coding: utf-8
 
